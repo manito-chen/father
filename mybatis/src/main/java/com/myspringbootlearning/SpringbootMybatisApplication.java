b72de07ea4bb1ad8bbd734049764a252ad862d6e
manito-chen <894772282@qq.com>
2021-04-10 00:41:12 +0800

    2021 4 10

--- mybatis/src/main/java/com/myspringbootlearning/SpringbootMybatisApplication.java
+++ mybatis/src/main/java/com/myspringbootlearning/SpringbootMybatisApplication.java
@@ -8,6 +8,7 @@ public class SpringbootMybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMybatisApplication.class, args);
+        System.out.println("++++++++++++++++++++++++"+SpringApplication.class.getClassLoader().getResource("META-INF/spring.factories"));
     }
 
 }
